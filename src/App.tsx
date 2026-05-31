1149806db2034324499b747c81f49ea67c1295cd
ViktorSkope <vikdopke@gmail.com>
2026-05-31 18:10:17 -0300

    anchoring bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
 import AboutPage from "./pages/AboutPage";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/work/:slug" element={<ProjectPage />} />
